3ce25d8915c52217c47f2b0b11ded87ba3b56bd8
litesh1998 <49069637+litesh1998@users.noreply.github.com>
2020-07-12 19:16:41 +0530

    reformat file

--- invisibility clock.py
+++ invisibility clock.py
@@ -2,29 +2,29 @@ import cv2
 import numpy as np
 import time
 
-webcam= cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(0)
 
 time.sleep(1)
-frame_count=0
-background=0
+frame_count = 0
+background = 0
 
 for i in range(60):
-    ret, background=webcam.read()
-background=np.flip(background, axis=1)
+    ret, background = webcam.read()
+background = np.flip(background, axis=1)
 
-while(webcam.isOpened()):
-    ret, img= webcam.read()
+while webcam.isOpened():
+    ret, img = webcam.read()
 
     if not ret:
         break
-    
-    frame_count+=1
 
-    img=np.flip(img, axis=1)
+    frame_count += 1
 
-    hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = np.flip(img, axis=1)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_red = np.array([100, 40, 40])
-    upper_red = np.array([100, 255,255])
+    upper_red = np.array([100, 255, 255])
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
 
     lower_red = np.array([155, 40, 40])
@@ -39,7 +39,7 @@ while(webcam.isOpened()):
     mask2 = cv2.bitwise_not(mask1)
 
     res2 = cv2.bitwise_and(img, img, mask=mask2)
-    
+
     res1 = cv2.bitwise_and(background, background, mask=mask1)
 
     finalOutput = cv2.addWeighted(res1, 1, res2, 1, 0)
